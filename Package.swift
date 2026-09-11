4d1ffeaf7eb142e0720a0099b03dfcfcf960811f
Volodymyr Andriienko <vandrjios@gmail.com>
2026-09-11 23:34:23 +0300

    Add SwiftSyntax dependencies to macro target

--- Package.swift
+++ Package.swift
@@ -25,6 +25,9 @@ let package = Package(
         .macro(
             name: "VAObscuredMacros",
             dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
+                .product(name: "SwiftDiagnostics", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
                 .product(name: "SwiftParser", package: "swift-syntax"),
